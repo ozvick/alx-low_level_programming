238601f5916661a5f5f2ec8a5718466645b1e1f0
ozvick <victoroyiboka@gmail.com>
2022-12-12 19:19:58 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
@@ -21,11 +21,10 @@ int main(void)
 				continue;
 			putchar(',');
 			putchar(' ');
-	   	}
+		}
 	}
 
 	putchar('\n');
 
 	return (0);
 }
-
